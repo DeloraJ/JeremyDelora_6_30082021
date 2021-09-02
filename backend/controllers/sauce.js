fbf10a45526ab9deca42a4b6159dfa0bc075dc3b
DeloraJ <84867062+DeloraJ@users.noreply.github.com>
2021-09-02 10:17:59 +0200

    Ajout fonction Delete

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -61,3 +61,18 @@ exports.modifySauce = (req, res, next) => {
     })
     .catch(error => res.status(500).json({ error }));
 };
+
+/* Fonction suppression sauce */
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+          .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
